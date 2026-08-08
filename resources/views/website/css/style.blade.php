e729b03c625864a7fa3ec1660db9309bd1538d28
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-08-08 20:16:25 +0530

    update the css

--- resources/views/website/css/style.blade.php
+++ resources/views/website/css/style.blade.php
@@ -2,27 +2,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
 @php
-    try {
-        // Vite se load karo (agar manifest exist kare)
-        $manifest = public_path('build/manifest.json');
-        if (file_exists($manifest)) {
-            $data = json_decode(file_get_contents($manifest), true);
-            $cssPath = $data['resources/css/website.css']['file'] ?? null;
-        } else {
-            $cssPath = null;
-        }
-    } catch (\Exception $e) {
-        $cssPath = null;
-    }
+    $cssFile = public_path('web_assets/css/style.css');
+    clearstatcache(true, $cssFile);
+    $cssVer = file_exists($cssFile) ? filemtime($cssFile) : '1.0';
 @endphp
-@if($cssPath)
-    <link rel="stylesheet" href="{{ asset('build/' . $cssPath) }}">
-@else
-    {{-- Fallback: old static CSS --}}
-    @php
-        $cssFile = public_path('web_assets/css/style.css');
-        clearstatcache(true, $cssFile);
-        $cssVer = file_exists($cssFile) ? filemtime($cssFile) : '1.0';
-    @endphp
-    <link rel="stylesheet" href="{{ asset('web_assets/css/style.css') }}?v={{ $cssVer }}">
-@endif
+<link rel="stylesheet" href="{{ asset('web_assets/css/style.css') }}?v={{ $cssVer }}">
