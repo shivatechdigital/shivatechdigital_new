db871a927068c5a96ac93ffcdd83ea282a2f0663
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-08-08 21:43:34 +0530

    fix: rename CSS to style-v2 to bypass LiteSpeed cache

--- resources/views/website/css/style.blade.php
+++ resources/views/website/css/style.blade.php
@@ -1,9 +1,4 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
-@php
-    $cssFile = public_path('web_assets/css/style.css');
-    clearstatcache(true, $cssFile);
-    $cssVer = file_exists($cssFile) ? filemtime($cssFile) : '1.0';
-@endphp
-<link rel="stylesheet" href="{{ asset('web_assets/css/style.css') }}?v={{ $cssVer }}">
+<link rel="stylesheet" href="{{ asset('web_assets/css/style-v2.css') }}">
