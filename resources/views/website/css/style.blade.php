ea07b8584f60a85b8363da8dddf117f3537df758
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-08-09 03:20:21 +0530

    fix: revert to style-v2.css

--- resources/views/website/css/style.blade.php
+++ resources/views/website/css/style.blade.php
@@ -1,4 +1,4 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
-<link rel="stylesheet" href="{{ asset('web_assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('web_assets/css/style-v2.css') }}">
